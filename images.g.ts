bc82012b01d8e007b409f5065c38a576108bb873
Probst1411 <quentin.pro99@gmail.com>
2025-04-09 18:22:23 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Code généré automatiquement. Ne pas modifier.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "JOEL":return img`
+. . . . . e e e e e . . . . . . 
+. . . . e e e e e e . . . . . . 
+. . e e e e e e e e e e . . . . 
+. . e e e e d d e e e e . . . . 
+. e e e e d d d d e e e . . . . 
+. . e f d d d d d d e e . . . . 
+. . . f d f d d f d d f . . . . 
+. . f f d d d d d d d f . . . . 
+f . f d f d d d d f d f . . f f 
+f f f f d f f f f d d f . f f . 
+. f f f d d d d d d d f f f . . 
+. . . f f f f d d d f f f . . . 
+. . . f . . f f d d f . f . . . 
+. . . f . . . f f f f . f . . . 
+. . . f . . . 3 f 3 . . f . . . 
+. . . f . . . . 3 . . . f . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Code généré automatiquement. Ne pas modifier.
